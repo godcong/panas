1f08f0aceec8d21994961d97c84e3a8bbdb8c6cb
godcong <jumbycc@163.com>
2020-04-28 03:09:36 +0800

    first add

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/godcong/panas/page"
+import (
+	"fmt"
+	"github.com/godcong/panas/page"
+)
 
 func main() {
 	//loop for handle new message
 	for {
 		for _, p := range page.List() {
+			fmt.Println("pa:", p)
 			page.GetPage(p)
 		}
 	}

8fd0e59e16c498e9ffe0222b267c6960fc9aba37
godcong <jumbycc@163.com>
2020-04-28 02:47:47 +0800

    first add

--- page/subpage.go
+++ page/subpage.go
@@ -9,6 +9,15 @@ import (
 
 var pageList = []string{
 	"https://www.nas2x.com/forums/synology-dsm/",
+	"https://www.nas2x.com/forums/other-nas/",
+	"https://www.nas2x.com/forums/linux/",
+	"https://www.nas2x.com/forums/router/",
+	"https://www.nas2x.com/forums/windows-server/",
+	"https://www.nas2x.com/forums/virtualization/",
+	"https://www.nas2x.com/forums/share/",
+	"https://www.nas2x.com/forums/11/",
+	"https://www.nas2x.com/forums/15/",
+	"https://www.nas2x.com/forums/2/",
 }
 
 func GetPage(url string) *goquery.Document {

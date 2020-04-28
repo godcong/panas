94878a4850ef9a798ab723868f07e0adaf9908f2
godcong <jumbycc@163.com>
2020-04-28 16:29:01 +0800

    update panas

--- page/subpage.go
+++ page/subpage.go
@@ -9,6 +9,7 @@ import (
 )
 
 var pageList = []string{
+	"https://www.nas2x.com/",
 	"https://www.nas2x.com/forums/synology-dsm/",
 	"https://www.nas2x.com/forums/other-nas/",
 	"https://www.nas2x.com/forums/linux/",
